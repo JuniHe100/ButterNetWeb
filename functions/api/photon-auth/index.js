58fa84060e61411a6428bb96b4d8c305f206b45b
JuniHe100 <mcliam838@gmail.com>
2026-09-13 18:20:14 -0400

    Add files via upload

---
+++ functions/api/photon-auth/index.js
@@ -0,0 +1,32 @@
+﻿export async function onRequestGet(context) {
+
+    const url = new URL(context.request.url);
+
+    const user =
+        url.searchParams.get("user") ||
+        url.searchParams.get("userid") ||
+        url.searchParams.get("userId") ||
+        "ButterNetGuest";
+
+    const build =
+        url.searchParams.get("build") ||
+        url.searchParams.get("version") ||
+        "unknown";
+
+    return new Response(JSON.stringify({
+        ResultCode: 1,
+        UserId: user,
+        Nickname: user,
+        Data: {
+            service: "ButterNet",
+            build: build,
+            butterNetPlus: true
+        }
+    }), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+            "Cache-Control": "no-store"
+        }
+    });
+}
