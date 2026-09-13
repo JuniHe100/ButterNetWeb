d87c19cb50fcf8502b173affd1f14bb7408c8a5d
JuniHe100 <mcliam838@gmail.com>
2026-09-13 12:53:33 -0400

    Add files via upload

---
+++ functions/api/health.js
@@ -0,0 +1,14 @@
+﻿export async function onRequestGet() {
+  return new Response(JSON.stringify({
+    ok: true,
+    service: "ButterNet API",
+    status: "online",
+    version: "1.0.0"
+  }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store"
+    }
+  });
+}
